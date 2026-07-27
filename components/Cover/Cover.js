1baffd8aea5b8812fe7e7e36bde397979ee50333
Louie Camacho <louiecamacho@gmail.com>
2026-07-27 17:50:06 -0500

    create heading component, util fonts.js

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
 export const Cover = ({children, background}) => {
-  return <div className="h-screen bg-slate-800 relative min-h-[400px] flex justify-center items-center">
+  return <div className="h-screen text-yellow-500 bg-slate-800 relative min-h-[400px] flex justify-center items-center">
     <Image 
     alt="Cover" 
     fill 
     className="object-cover mix-blend-soft-light" 
     src={background}/>
-    {children}</div>;
+      <div >
+        {children}
+      </div>
+    </div>;
 };
