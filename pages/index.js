d13b7278641c44f85932936194aa07e6584fc34e
Louie Camacho <louiecamacho@gmail.com>
2026-07-28 19:54:16 -0500

    fix component reference

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 import client from "client";
-import { BlockRenderer } from "components/BlockRenderer/BlockRenderer";
+import { BlockRenderer } from "components/BlockRenderer";
 import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
 
 export default function Home(props) {
