7a2f0732a3aedf745d247d7ee7b3244ec4e65963
Louie Camacho <louiecamacho@gmail.com>
2026-07-29 14:05:03 -0500

    map menu items and sub menu items

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { gql } from "@apollo/client";
 import client from "client";
 import { BlockRenderer } from "components/BlockRenderer";
 import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+import { mapMenuItems } from "utils/mapMainMenu";
 
 export default function Home(props) {
   console.log("PROPS: ",props);
@@ -14,11 +15,35 @@ export default function Home(props) {
 export const getStaticProps = async () => {
   const {data} = await client.query({
     query: gql`
-    query NewQuery {
+    query PageQuery {
       nodeByUri(uri: "/") {
         ... on Page {
           id
           blocks(postTemplate: false)
+        }
+      }
+      acfOptionsMainMenu {
+        mainMenu {
+          menuItems {
+            menuItem {
+              destination {
+                ... on Page {
+                  id
+                  uri
+                } 
+              }
+              label
+            }
+            items {
+              destination {
+                ... on Page {
+                  id
+                  uri
+                }
+              }
+              label
+            }
+          }
         }
       }
     }`
@@ -26,6 +51,7 @@ export const getStaticProps = async () => {
 
   return {
     props: {
+      mainMenuItems: mapMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
       blocks: cleanAndTransformBlocks(data.nodeByUri.blocks)
     },
   };
