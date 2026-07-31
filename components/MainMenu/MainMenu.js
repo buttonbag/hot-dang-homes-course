8cfa781c8791a631d802f6a0001985150bfdae06
Louie Camacho <louiecamacho@gmail.com>
2026-07-31 09:22:14 -0500

    complete main menu

---
+++ components/MainMenu/MainMenu.js
@@ -0,0 +1,39 @@
+
+import Link from 'next/link';
+import { FaHouseUser } from 'react-icons/fa';
+
+export const MainMenu = ({ items, callToActionLabel, callToActionDestination }) => {
+  console.log("MENU: ", items);
+
+  return <div className="bg-slate-800 sticky top-0 z-20 h-[64px] px-5 text-white flex">
+    <div className="py-4 pl-5 flex text-pink-600">
+      <div className='flex'>
+        <FaHouseUser size={30} />
+        Hot Dang Homes
+      </div>
+    </div>
+    <nav className='flex flex-1 justify-end'>
+      {(items || []).map(item => (
+        <div key={item.id} className='hover:bg-slate-500 relative group'>
+          <Link href={item.destination} className="p-5 block">{item.label}</Link>
+          {!!item.subMenuItems?.length && (
+            <div className='group-hover:block hidden bg-slate-800 text-right absolute right-0 top-full -mt-3'>
+              {item.subMenuItems.map(subItem => (
+                  <Link key={subItem.id} href={subItem.destination} className='hover:bg-slate-500 p-5 block whitespace-nowrap' >
+                    {subItem.label}
+                  </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+      <div className='ml-3 my-auto'>
+        <Link 
+        href={callToActionDestination}
+        className='bg-pink-500 hover:bg-pink-700 inline-block my-2 px-4 py-2 uppercase rounded-md cursor-pointer font-bold text-white'>
+          {callToActionLabel}
+        </Link>
+      </div>
+    </nav>
+  </div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import { gql } from "@apollo/client";
 import client from "client";
 import { BlockRenderer } from "components/BlockRenderer";
+import { MainMenu } from "components/MainMenu";
 import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
 import { mapMenuItems } from "utils/mapMainMenu";
 
 export default function Home(props) {
   console.log("PROPS: ",props);
   
-  return <div>
+  return <div className="font-body">
+    <MainMenu items={props.mainMenuItems} callToActionLabel={props.callToActionLabel} callToActionDestination={props.callToActionDestination} />
     <BlockRenderer blocks={props.blocks}/>
   </div>;
 }
@@ -24,6 +26,14 @@ export const getStaticProps = async () => {
       }
       acfOptionsMainMenu {
         mainMenu {
+          callToActionButton {
+            label
+            destination {
+              ... on Page {
+                uri
+              }
+            }
+          }        
           menuItems {
             menuItem {
               destination {
@@ -52,7 +62,9 @@ export const getStaticProps = async () => {
   return {
     props: {
       mainMenuItems: mapMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
-      blocks: cleanAndTransformBlocks(data.nodeByUri.blocks)
+      blocks: cleanAndTransformBlocks(data.nodeByUri.blocks),
+      callToActionLabel: data.acfOptionsMainMenu.mainMenu.callToActionButton.label,
+      callToActionDestination: data.acfOptionsMainMenu.mainMenu.callToActionButton.destination.uri,
     },
   };
 };
